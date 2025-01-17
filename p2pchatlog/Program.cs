174000dd92f0ce55d3cfede3ad5957880da0eba1
David Stall <davidstall312@gmail.com>
2025-01-17 14:56:36 -0800

    push

--- p2pchatlog/Program.cs
+++ p2pchatlog/Program.cs
@@ -12,37 +12,22 @@ class Program
     static void Main(string[] args)
     {
         int port = 64005;
-        Console.WriteLine("Press c!");
-        string serverOrClient = Console.ReadLine();
-        if (serverOrClient == "c")
-        {
-            Console.WriteLine("Starting client...");
-            Console.WriteLine("What is the server's local IPv4 address? (open CMD, type \"ipconfig\" to find out.)");
-            PeerClient client = new PeerClient(Console.ReadLine(), port);
-            client.StartListening();
+        // server
+        Console.WriteLine("Starting Server...");
+        PeerServer server = new PeerServer(port);
+        server.Start();
+        // client
+        Console.WriteLine("Starting Client");
 
-            Console.WriteLine("Enter message to send (or type \"exit\" to quit");
-            string message;
-            while ((message = Console.ReadLine()) != "exit")
-            {
-                client.SendMessage(message);
-            }
-            client.Close();
-        }
-        else if (serverOrClient == "s")
+        Console.WriteLine("What is the server's local IPv4 address? (open CMD, type \"ipconfig\" to find out.)");
+        PeerClient client = new PeerClient(Console.ReadLine(), port);
+        client.StartListening();
+        string message;
+        while ((message = Console.ReadLine()) != "exit")
         {
-            Console.WriteLine("Starting server...");
-            PeerServer server = new PeerServer(port);
-            server.Start();
-
-            Console.WriteLine("Press any key to stop the server...");
-            Console.ReadKey();
-            server.Stop();
+            client.SendMessage(message);
         }
-        else
-        {
-            Console.WriteLine("Invalid input");
-        }
+        client.Close();
     }
 }
 
--- p2pchatlog/senderReciever.cs
+++ p2pchatlog/senderReciever.cs
@@ -1,28 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace p2pchatlog 
+namespace p2pchatlog
 {
     class PeerClient
     {
         private TcpClient client;
         private NetworkStream stream;
-        private TcpListener listener;
-        private bool isRunning;
-        int clientsConnected = 0;
 
         public PeerClient(string serverIp, int serverPort)
         {
             client = new TcpClient(serverIp, serverPort);
             stream = client.GetStream();
-
-            listener = new TcpListener(IPAddress.Any, serverPort);
-            isRunning = false;
         }
 
         public void StartListening()
@@ -30,12 +23,6 @@ namespace p2pchatlog
             Thread listenThread = new Thread(ListenForMessages);
             listenThread.IsBackground = true;
             listenThread.Start();
-            listener.Start();
-            isRunning = true;
-            Console.WriteLine("Server is listening for incoming connections...");
-
-            Thread listenerThread = new Thread(ListenForConnections);
-            listenerThread.Start();
         }
 
         private void ListenForMessages()
@@ -53,57 +40,12 @@ namespace p2pchatlog
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine("Error while receiving message: " + ex.Message); 
+                    Console.WriteLine("Error while receiving message: " + ex.Message);
                     break;
                 }
             }
         }
-        private void ListenForConnections()
-        {
-            while (isRunning)
-            {
-                try
-                {
-                    var client = listener.AcceptTcpClient();
-                    Console.WriteLine("Client Connected");
-                    clientsConnected++;
-                    HandleClient(client);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Error accepting connection " + ex.Message);
-                }
-            }
-        }
-        private void HandleClient(TcpClient client)
-        {
-            NetworkStream stream = client.GetStream();
-            byte[] buffer = new byte[1024];
-
-            try
-            {
-                while (true)
-                {
-                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
-                    Console.WriteLine(bytesRead);
-                    if (bytesRead != 0)
-                    {
-                        string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-                        Console.WriteLine("Message Received from client: " + message);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error while reading from stream " + ex.Message);
-            }
-            finally
-            {
-                Console.WriteLine("Closed client connection");
-                client.Close();
-            }
-        }
-            public void SendMessage(string message)
+        public void SendMessage(string message)
         {
             byte[] buffer = Encoding.UTF8.GetBytes(message);
             stream.Write(buffer, 0, buffer.Length);
